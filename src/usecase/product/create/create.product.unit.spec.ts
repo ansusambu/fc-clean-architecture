db537eeb2b5c66fc508af9caa9f736a69401d444
ansusambu <ansusambu@gmail.com>
2025-01-14 23:10:59 -0300

    desafio Notification pattern

--- src/usecase/product/create/create.product.unit.spec.ts
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -35,7 +35,7 @@ describe("Unit test create product use case", () => {
     };
 
     await expect(productCreateUseCase.execute(input)).rejects.toThrow(
-      "Name is required"
+      "product: Name is required"
     );
   });
 
@@ -48,7 +48,7 @@ describe("Unit test create product use case", () => {
     };    
 
     await expect(productCreateUseCase.execute(input)).rejects.toThrow(
-      "Price must be greater than zero"
+      "product: Price must be greater than zero"
     );
   });
 });
